631d6782963022a7272ca416ace699afe66b2eb3
Matheus Berger <39223854+bergermatheus@users.noreply.github.com>
2021-05-25 01:01:20 -0300

    Create route to allow client authentication
    This route is used to authenticate the client. The client sends the password in the authorization header. The server returns a temporary token which the client should use as a Bearer token for authentication when doing the other API requests.

--- src/app.py
+++ src/app.py
@@ -11,6 +11,17 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['JSON_SORT_KEYS'] = False
 app.secret_key = os.environ.get('SECRET_KEY','dev')
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
 if __name__ == "__main__":
 
 
